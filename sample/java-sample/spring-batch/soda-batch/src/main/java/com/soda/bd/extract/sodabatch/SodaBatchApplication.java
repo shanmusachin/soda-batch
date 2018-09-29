8e1589007b8f78e251f8d420b5602399ae5ab936
shanmugamvairavan <shanmusachin@gmail.com>
2018-09-29 17:12:19 -0500

    First Batch

---
+++ sample/java-sample/spring-batch/soda-batch/src/main/java/com/soda/bd/extract/sodabatch/SodaBatchApplication.java
@@ -0,0 +1,12 @@
+package com.soda.bd.extract.sodabatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodaBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SodaBatchApplication.class, args);
+	}
+}
